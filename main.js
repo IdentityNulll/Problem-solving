261c47e1831cb32238d60c68b9112cd7afdd0a35
Identitynull <identitynulll@gmail.com>
2025-12-17 08:17:49 +0500

    added

---
+++ main.js
@@ -0,0 +1,78 @@
+// 1. finding the total value of odd numbers from 1 to 100
+// let totalNumber = 0
+// for(let i = 1; i<=100; i++) {
+
+//     if (i%2==1) {
+//         totalNumber+=i;
+//     }
+// }
+// console.log(totalNumber)
+
+// 2. reversing the string(prompt) the user enters using for
+// let Prompt = prompt("Enter something whatever it is")
+// let reversed = ""
+
+// for (let i = Prompt.length - 1; i >= 0; i--) {
+//     reversed += Prompt[i]
+// }
+// console.log(reversed)
+
+// 3. Returning the "Hello world!" 10 times
+// for (let i = 1; i <= 10; i++) {
+//     console.log("Hello world")
+// }
+
+// 4. Palindrom 
+// let promptValue = prompt("Enter any word!")
+// let reversed = ""
+
+// for (let i = promptValue.length -1; i >=0 ; i--) {
+//     reversed += promptValue[i]
+// }
+
+// if (promptValue === reversed ) {
+//     console.log("Palindrom")
+// }else {
+//     console.log("Not Palindrom")
+// }
+
+// 5. Returning only the upparCase letters
+// let text = prompt("Enter Anything with upparCase")
+// let result = ""
+
+
+// for (let i = 0; i <= text.length - 1; i++) {
+//     if (text[i] === text[i].toUpperCase()) {
+//         result += text[i]
+//     }
+// }
+
+// console.log(result)
+
+// 6. Returning two of all the characters
+// let text = "Enter anything ig"
+// let result = ""
+
+// for (let i = 0; i < text.length -1; i++) {
+//     result += text[i].repeat(2)
+// }
+// console.log(result)
+
+// 7. returning how many "a" are in the string
+// let text = "what the heck brahh aaaaa"
+// let result = ""
+
+// for (let i = 0; i <= text.length - 1; i++ ) {
+//     if (text[i] === "a") {
+//         result += text[i]
+//     }
+// }
+
+// console.log(result.length)
+
+// 8. returing * instead of some of the emails
+let text = "identitynull@gmail.com"
+
+for (let i = 0; i <=text.length -1; i++ ) {
+    
+}
